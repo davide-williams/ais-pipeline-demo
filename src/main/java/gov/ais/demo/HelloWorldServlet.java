9828cf77e3bde08d9cf5c42682d55ee1af49e66c
davide-williams <DavidE.Williams@vita.virginia.gov>
2023-01-27 19:35:44 -0500

    updates

--- src/main/java/gov/ais/demo/HelloWorldServlet.java
+++ src/main/java/gov/ais/demo/HelloWorldServlet.java
@@ -1,22 +1,25 @@
 package gov.ais.demo;
 
-
-import java.io.*; 
+import java.io.*;
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
-
 
 @SuppressWarnings("serial")
 @WebServlet(name = "HelloServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
 
-	 public void doGet(HttpServletRequest request, 
-	          HttpServletResponse response) 
-	          throws ServletException, IOException 
-	{
-	 response.setContentType("text/html");
-	    PrintWriter out = response.getWriter();
-	    out.println("Hello World"); 
-	  }
+	/**
+	 * Do get.
+	 *
+	 * @param request the request
+	 * @param response the response
+	 * @throws ServletException the servlet exception
+	 * @throws IOException Signals that an I/O exception has occurred.
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("Hello from the AIS demo servlet !!!!");
+	}
 }
