2c76e8f6ca8b275f32432fa8691184b595ba69c7
davide-williams <DavidE.Williams@vita.virginia.gov>
2023-01-27 19:32:17 -0500

    updates

--- src/main/java/gov/ais/demo/HelloWorldServlet.java
+++ src/main/java/gov/ais/demo/HelloWorldServlet.java
@@ -1,6 +1,22 @@
 package gov.ais.demo;
 
-public class HelloWorldServlet {
 
-	hello;
+import java.io.*; 
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+
+@SuppressWarnings("serial")
+@WebServlet(name = "HelloServlet", urlPatterns = "/hello")
+public class HelloWorldServlet extends HttpServlet {
+
+	 public void doGet(HttpServletRequest request, 
+	          HttpServletResponse response) 
+	          throws ServletException, IOException 
+	{
+	 response.setContentType("text/html");
+	    PrintWriter out = response.getWriter();
+	    out.println("Hello World"); 
+	  }
 }
